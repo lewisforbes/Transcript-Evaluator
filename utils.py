b0c024280295fd6cb45c18e1c431d63efeafd88f
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2024-06-28 16:22:34 +0100

    minor changes

--- utils.py
+++ utils.py
@@ -4,13 +4,18 @@ from os import path
 import os
 import sys
 
+# for get_accuracy()
 from werpy import normalize
 from jiwer import wer
 
 # gets text content from a VTT or SRT file
-# https://github.com/lewisforbes/VTT-to-TXT
+# https://github.com/ lewisforbes/VTT-to-TXT
 def get_sub_contents(subtitle_fpath):
     with open(subtitle_fpath, "r", encoding='utf-8') as f:
+        # return full body of text file
+        if path.splitext(subtitle_fpath)[1]==".txt":
+            return sub(" +", " ", f.read().replace("\n", " ")) # remove multiple spaces
+        
         output = ""
         next = False
         for line in f:
@@ -28,20 +33,20 @@ def get_sub_contents(subtitle_fpath):
 
     # TODO move in loop
     # fix up output
-    output = re.sub("\[.+?\]", "", output) # remove speaker/context tags
-    output = re.sub("  +", " ", output) # remove multiple spaces
+    output = sub("\[.+?\]", "", output) # remove speaker/context tags
+    output = sub("  +", " ", output) # remove multiple spaces
     return output
 
 # calculates the word error rate (expressed as an accuracy) between two transcripts.
 # gtrans: generated transcript
 # ctrans: correct transcript
 def get_accuracy(gtrans, ctrans):
-    return 1 - wer(normalize(ctrans), normalize(gtrans))
+    return 1 - min(1, wer(normalize(ctrans), normalize(gtrans))) # wer>1 possible (https://w.wiki/_sXTY)
 
 
 # returns true iff a file is a subtitle file
 def is_subtitle(fpath):
-    return path.splitext(fpath)[1] in [".srt", ".vtt"]
+    return path.splitext(fpath)[1] in [".txt", ".srt", ".vtt"]
 
 # returns subset of os.listdir for which all subdirnames are numeric only when num_only=True
 def list_video_dirs(data_dir, num_only):
@@ -63,10 +68,11 @@ def list_video_dirs(data_dir, num_only):
 # returns true iff two file contents are different
 def contents_different(p1, p2):
     if p1==p2: return False
+    return get_sub_contents(p1)!=get_sub_contents(p2)
     
     with open(p1, "r") as f1:
         with open(p2, "r") as f2:
-            return f1.read()!=f2.read()
+            return get_sub_contents(f1.read())!=get_sub_contents(f2.read())
         
 
 def error(msg):
